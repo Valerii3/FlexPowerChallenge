7a607baa258acf5b835a2672e504e67346828a1f
Valerii <vallerus55@gmail.com>
2024-10-09 14:47:28 +0200

    add database and basic auth

--- task1/app.py
+++ task1/app.py
@@ -1,14 +1,34 @@
+import os
 from datetime import datetime
 
 from flask import Flask, jsonify, request
-from sqlalchemy import SQLAlchemy
-from models import db
+
+from extensions import db, ma
 from task1.models import Trade, trades_schema
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+USER_DATA = {
+    "Username": "Password"
+}
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'trades.db')
+
+db.init_app(app)
+ma.init_app(app)
 
 
 @app.route('/v1/trades', methods=['GET'])
+@auth.login_required
 def get_trades():
     trader_id = request.args.get('trader_id')
     delivery_day = request.args.get('delivery_day')
@@ -29,6 +49,7 @@ def get_trades():
 
 
 @app.route('/v1/trades', methods=['POST'])
+@auth.login_required
 def post_trade():
     trade_data = request.json
 
@@ -50,3 +71,5 @@ def post_trade():
         return jsonify({'status_code': 400, 'message': str(e)}), 400
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- task1/models.py
+++ task1/models.py
@@ -1,13 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy import Column, Integer, String, DateTime
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from marshmallow import Schema, fields, validate
-from flask_marshmallow import Marshmallow
+from flask_marshmallow import Marshmallow, Schema, fields, validate
+from extensions import db, ma
 
-from app import app
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
 
 class Trade(db.Model):
@@ -24,25 +21,9 @@ class Trade(db.Model):
 
 
 class TradeSchema(Schema):
-    id = fields.String(
-        required=True, description="Unique id of the trade as defined by the exchange", example="trade_123")
-    price = fields.Integer(
-        required=True, description="Price in eurocent/MWh.", example=200)
-    quantity = fields.Integer(
-        required=True, description="Quantity in MW.", example=12)
-    direction = fields.String(
-        required=True,
-        validate=validate.Regexp('^(buy|sell)$'),
-        description="Direction of the trade from the perspective of flew-power, can be either buy or sell."
-    )
-    delivery_day = fields.Date(
-        required=True, description="Day on which the energy has to be delivered in local time.")
-    delivery_hour = fields.Integer(
-        required=True, description="Hour during which the energy has to be delivered in local time.", example=14)
-    trader_id = fields.String(
-        required=True, description="Unique id of a trader (bot or team member).", example="MirkoT")
-    execution_time = fields.DateTime(
-        required=True, description="UTC datetime at which the trade occurred on the exchange.")
+    class Meta:
+        fields = ('id', 'price', 'quantity', 'direction', 'delivery_day', 'delivery_hour', 'trader_id', 'execution_time')
+
 
 
 trade_schema = TradeSchema()
